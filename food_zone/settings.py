047bfd75d729894c3c600a2925dd5171be48a80d
Ashik Aslam <158561158+ashikaslam@users.noreply.github.com>
2024-02-29 09:34:03 +0600

    abc

--- food_zone/settings.py
+++ food_zone/settings.py
@@ -129,14 +129,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+STATIC_URL = 'static/'
+
+
+
 STATICFILES_DIRS = [
     BASE_DIR/'static',
 ]
+
+
 
 
 
