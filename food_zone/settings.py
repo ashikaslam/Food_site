3be6171a5c8c89739004eba360fd5aa0164d7f20
Ashik Aslam <158561158+ashikaslam@users.noreply.github.com>
2024-02-29 12:43:26 +0600

    Your commit message here

--- food_zone/settings.py
+++ food_zone/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=x*zxc9jj%^o8s5im_e4@7g--mcb_dkv=kv$z705xj%6h570t%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
