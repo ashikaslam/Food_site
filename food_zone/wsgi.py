c8d78e4a3366a500f138d6b1064cc74136e21832
Ashik Aslam <158561158+ashikaslam@users.noreply.github.com>
2024-02-29 12:35:35 +0600

    Your commit message here

--- food_zone/wsgi.py
+++ food_zone/wsgi.py
@@ -14,3 +14,21 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_zone.settings')
 
 application = get_wsgi_application()
+"""
+WSGI config for food_zone project.
+
+It exposes the WSGI callable as a module-level variable named `application`.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_zone.settings')
+
+# Use Gunicorn as the application server
+# from whitenoise import WhiteNoise
+# application = get_wsgi_application()
+# application = WhiteNoise(application)
